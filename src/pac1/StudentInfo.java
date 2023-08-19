084745b83df7731080b54002c76359c46d3a246c
MayMeHlwanMyo <thisbe980606@gmail.com>
2023-08-19 23:16:43 +0630

    Version2.1

--- src/pac1/StudentInfo.java
+++ src/pac1/StudentInfo.java
@@ -5,6 +5,7 @@ public class StudentInfo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Student Pls fill the form for new student");
+		System.out.println("practicing is the best");
 	}
 
 }
